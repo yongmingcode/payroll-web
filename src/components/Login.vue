b6f0c8c0b6e63de634a04d3b026e7ff8c4ce3ad6
mayongming <240401461@qq.com>
2019-12-04 22:43:40 +0800

    渲染Login组件，并实现路由重定向

---
+++ src/components/Login.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+      登陆组建
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Login from '../components/Login.vue'
 
 Vue.use(Router)
 
@@ -8,8 +8,11 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      redirect: '/login'
+    },
+    {
+      path: '/login',
+      component: Login
     }
   ]
 })
