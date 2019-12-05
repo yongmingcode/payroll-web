2e7a07b60582ed6afae011878bb01b031fc7e9f1
mayongming <240401461@qq.com>
2019-12-05 23:26:45 +0800

    Login页面input框，但是element-ui样式不生效，待解决

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,7 +1,28 @@
 <template>
-    <div class="login_container">
-      <div class="login_box"></div>
+  <div class="login_container">
+    <div class="login_box">
+      <!--头像区-->
+      <div class="avatar_box">
+        <img src="../assets/img/vuelogo.png" alt=""/>
+      </div>
+      <!--登陆表单区-->
+      <el-form label-width="0px" class="login_form">
+        <!--用户名-->
+        <el-form-item>
+          <el-input ></el-input>
+        </el-form-item>
+        <!--密码-->
+        <el-form-item>
+          <el-input ></el-input>
+        </el-form-item>
+        <!--按钮-->
+        <el-form-item class="btns">
+          <el-button type="primary">登陆</el-button>
+          <el-button type="info">重置</el-button>
+        </el-form-item>
+      </el-form>
     </div>
+  </div>
 </template>
 
 <script>
@@ -9,11 +30,12 @@ export default {}
 </script>
 
 <style lang="less" scoped>
-  .login_container{
+  .login_container {
     background-color: #2d4d6d;
     height: 100%;
   }
-  .login_box{
+
+  .login_box {
     width: 450px;
     height: 300px;
     background-color: #fff;
@@ -21,6 +43,38 @@ export default {}
     position: absolute;
     left: 50%;
     top: 50%;
-    transform: translate(-50%,-50%);
+    transform: translate(-50%, -50%);
+
+    .avatar_box {
+      width: 130px;
+      height: 130px;
+      border: 1px solid #eee;
+      border-radius: 50%;
+      padding: 10px;
+      box-shadow:  0 0 10px #eee;
+      position: absolute;
+      left: 50%;
+      transform: translate(-50%,-50%);
+      background-color: #fff;
+      img {
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+        background-color: #eee;
+      }
+    }
+
+    .login_form{
+      position: absolute;
+      bottom: 0;
+      width: 100%;
+      padding: 0 20px;
+      box-sizing: border-box;
+    }
+
+    .btns{
+      display: flex;
+      justify-content: flex-end;
+    }
   }
 </style>
