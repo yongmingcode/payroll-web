7e17149d17e507d4dcf9b4fb1969d1f9357ba919
yongmingcode <240401461@qq.com>
2019-12-06 10:44:28 +0800

    解决全局element样式不对的问题

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import './plugins/element.js'
+import 'element-ui/lib/theme-chalk/index.css'
 
 // 导入全局样式
 import './assets/css/global.css'
