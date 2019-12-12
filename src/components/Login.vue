4b11b9767854a2cd466e18646f4f142dee6c6f43
mayongming <240401461@qq.com>
2019-12-12 21:16:39 +0800

    解决eslint与webstorm格式化（ctrl+alt+l）冲突的问题。1.缩进，2.方法的小括号前eslint需要加空格，3.引号改为单引号，4.结尾不要分号

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -26,55 +26,56 @@
 </template>
 
 <script>
-import qs from 'qs'
-export default {
-  data () {
-    return {
-      // 这是登陆表单得数据绑定
-      loginForm: {
-        account: 'admin',
-        password: 'admin'
+  import qs from 'qs'
+
+  export default {
+    data() {
+      return {
+        // 这是登陆表单得数据绑定
+        loginForm: {
+          account: 'admin',
+          password: 'admin'
+        },
+        // 这是表单验证规则对象
+        loginFormRules: {
+          // 验证用户名是否合法
+          account: [{required: true, message: '请输入登陆名称', trigger: 'blur'},
+            {min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur'}
+          ],
+          // 验证密码是否合法
+          password: [{required: true, message: '请输入登陆名称', trigger: 'blur'},
+            {min: 3, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur'}
+          ]
+        }
+      }
+    },
+    methods: {
+      // 点击重置按钮，重置登陆表单
+      resetLoginForm() {
+        // console.log(this)''
+        this.$refs.loginFormRef.resetFields()
       },
-      // 这是表单验证规则对象
-      loginFormRules: {
-        // 验证用户名是否合法
-        account: [{required: true, message: '请输入登陆名称', trigger: 'blur'},
-          {min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur'}
-        ],
-        // 验证密码是否合法
-        password: [{required: true, message: '请输入登陆名称', trigger: 'blur'},
-          {min: 3, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur'}
-        ]
+      login() {
+        this.$refs.loginFormRef.validate(async valid => {
+          if (!valid) return
+          const {data: res} = await this.$http.post('login', qs.stringify({
+            account: this.loginForm.account,
+            password: this.loginForm.password
+          }))
+          if (res.code !== 0) return this.$message.error('登陆失败')
+          this.$message.success('登陆成功')
+
+          // 1.登陆成功后的token，保存到客户端的sessionStorage中
+          //   1.1 项目中除了登陆之外的其它API接口，必须在登陆之后才能访问
+          //   1.2 token只应在当前网站打开期间生效（sessionStorage是会话存储机制的原因），
+          //   所以将token保存在sessionStorage中(不用LocalStorage，是因为不用LocalStorage是持久化的存储机制，)
+          window.sessionStorage.setItem('token', res.data.token)
+          // 2.通过编程式导航跳转到后台主页，路由地址是 /home
+          this.$router.push('/home')
+        })
       }
     }
-  },
-  methods: {
-    // 点击重置按钮，重置登陆表单
-    resetLoginForm () {
-      // console.log(this)''
-      this.$refs.loginFormRef.resetFields()
-    },
-    login () {
-      this.$refs.loginFormRef.validate(async valid => {
-        if (!valid) return
-        const {data: res} = await this.$http.post('login', qs.stringify({
-          account: this.loginForm.account,
-          password: this.loginForm.password
-        }))
-        if (res.code !== 0) return this.$message.error('登陆失败')
-        this.$message.success('登陆成功')
-
-        // 1.登陆成功后的token，保存到客户端的sessionStorage中
-        //   1.1 项目中除了登陆之外的其它API接口，必须在登陆之后才能访问
-        //   1.2 token只应在当前网站打开期间生效（sessionStorage是会话存储机制的原因），
-        //   所以将token保存在sessionStorage中(不用LocalStorage，是因为不用LocalStorage是持久化的存储机制，)
-        window.sessionStorage.setItem('token', res.data.token)
-        // 2.通过编程式导航跳转到后台主页，路由地址是 /home
-        this.$router.push('/home')
-      })
-    }
   }
-}
 </script>
 
 <style lang="less" scoped>
