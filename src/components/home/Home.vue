0a97edaf47a1e651308260c728ba6badefacbd9e
mayongming <240401461@qq.com>
2019-12-13 21:23:26 +0800

    侧边栏路由的改造

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -14,7 +14,7 @@
         <div class="toggle-button" @click="toggleCollapse"> < > </div>
         <!--侧边栏菜单区域-->
         <el-menu background-color="#333744" text-color="#fff" active-text-color="#409EFF" :unique-opened="true"
-        :collapse="isCollapse" :collapse-transition="false">
+        :collapse="isCollapse" :collapse-transition="false" :router="true">
           <!--一级菜单-->
           <el-submenu :index="item.id + ''" v-for="item in menulist" :key="item.id">
             <!--一级菜单的模板区-->
@@ -26,7 +26,7 @@
             </template>
 
             <!--二级菜单-->
-            <el-menu-item :index="subItem.id + ''" v-for="subItem in item.childrenMenu" :key="subItem.id"><!--图标-->
+            <el-menu-item :index="'/' + subItem.path" v-for="subItem in item.childrenMenu" :key="subItem.id"><!--图标-->
               <i class="el-icon-menu"></i>
               <!--文本-->
               <span>{{subItem.name}}</span></el-menu-item>
