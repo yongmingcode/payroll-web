c5f37fc2dc2e41ecce8e7e7cc5c2d9e93156d800
mayongming <240401461@qq.com>
2019-12-13 22:42:02 +0800

    管理员列表页面：面包屑导航去、搜索框、添加按钮 、用户列表的参数对象

--- src/components/manager/Manager.vue
+++ src/components/manager/Manager.vue
@@ -1,32 +1,58 @@
 <template>
-    <div>
-      <!--面包屑导航区-->
-      <el-breadcrumb separator-class="el-icon-arrow-right">
-        <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
-        <el-breadcrumb-item>管理员管理</el-breadcrumb-item>
-        <el-breadcrumb-item>管理员列表</el-breadcrumb-item>
-      </el-breadcrumb>
+  <div>
+    <!--面包屑导航区-->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>管理员管理</el-breadcrumb-item>
+      <el-breadcrumb-item>管理员列表</el-breadcrumb-item>
+    </el-breadcrumb>
 
-      <!--卡片试图区域-->
-      <el-card>
-        <!--搜索与添加区域-->
-        <el-row :gutter="20">
-          <el-col :span="8">
-            <el-input placeholder="请输入内容">
-              <el-button slot="append" icon="el-icon-search"></el-button>
-            </el-input>
-          </el-col>
-          <el-col :span="2">
-            <el-button type="primary">添加管理员</el-button>
-          </el-col>
-        </el-row>
-      </el-card>
-    </div>
+    <!--卡片试图区域-->
+    <el-card>
+      <!--搜索与添加区域-->
+      <el-row :gutter="20">
+        <el-col :span="8">
+          <el-input placeholder="请输入内容">
+            <el-button slot="append" icon="el-icon-search"></el-button>
+          </el-input>
+        </el-col>
+        <el-col :span="2">
+          <el-button type="primary">添加管理员</el-button>
+        </el-col>
+      </el-row>
+    </el-card>
+  </div>
 </template>
 
 <script>
-    export default {
+  export default {
+    data() {
+      return {
+        // 获取用户列表的参数对象
+        queryInfo: {
+          query: '',
+          pageNum: 1,
+          pagesize: 2
+        },
+        managerList: [],
+        total: 0
+      }
+    },
+    created() {
+      this.getManagerList()
+    },
+    methods: {
+      async getManagerList() {
+        const {data: res} = await this.$http.get('yl/manager/getManagerList', {
+          params: this.queryInfo
+        })
+        if (res.code !== 0) return this.$message.error('获取管理员信息失败！')
+        this.managerList = res.data.managerList
+        this.total = res.data.total
+        console.log(res)
+      }
     }
+  }
 </script>
 
 <style lang="less" scoped>
