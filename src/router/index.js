bee0bfe9f38cf7d19668527dbb5894f1efab8288
mayongming <240401461@qq.com>
2019-12-13 21:17:05 +0800

    重定向到home中welcome

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '../components/Login.vue'
 import Home from '../components/home/Home.vue'
+import Welcome from '../components/home/welcome/Welcome.vue'
 
 Vue.use(Router)
 
@@ -9,7 +10,12 @@ const router = new Router({
   routes: [
     {path: '/', redirect: '/login'},
     {path: '/login', component: Login},
-    {path: '/home', component: Home}
+    {
+      path: '/home',
+      component: Home,
+      redirect: '/welcome',
+      children: [{path: '/welcome', component: Welcome}]
+    }
   ]
 })
 
