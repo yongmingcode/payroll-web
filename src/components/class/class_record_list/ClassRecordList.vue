2518f46b6e2c5c220bf80b931548905a9b60e179
mayongming <240401461@qq.com>
2019-12-23 22:08:13 +0800

    课程记录列表数据展示，添加功能完成一半

--- src/components/class/class_record_list/ClassRecordList.vue
+++ src/components/class/class_record_list/ClassRecordList.vue
@@ -1,14 +1,181 @@
 <template>
-    <div>
-      classrecordlist
-    </div>
+  <div>
+    <!--面包屑导航区-->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>课程管理</el-breadcrumb-item>
+      <el-breadcrumb-item>课程记录列表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!--卡片区域-->
+    <el-card class="box-card">
+      <el-row :gutter="20">
+        <el-col :span="8">
+          <el-input placeholder="请输入内容">
+            <el-button slot="append" icon="el-icon-search"></el-button>
+          </el-input>
+        </el-col>
+        <el-col :span="2">
+          <el-button type="primary" @click="addDialogVisible = true">添加管理员</el-button>
+        </el-col>
+      </el-row>
+
+      <!--课程记录列表区域-->
+      <el-table :data="classRecordList" border stripe>
+        <el-table-column label="记录编号" prop="id"></el-table-column>
+        <el-table-column label="上课地点ID" prop="locationId"></el-table-column>
+        <el-table-column label="课堂名称" prop="className"></el-table-column>
+        <el-table-column label="课堂类型ID" prop="classTypeId"></el-table-column>
+        <el-table-column label="课时" prop="classPeriod"></el-table-column>
+        <el-table-column label="上课具体时间" :formatter="dateFormat" prop="classTime"></el-table-column>
+        <el-table-column label="时长（单位：分钟）" prop="duration"></el-table-column>
+        <el-table-column label="课堂备注" prop="content"></el-table-column>
+        <el-table-column label="创建时间" :formatter="dateFormat" prop="createTime"></el-table-column>
+        <el-table-column label="操作" width="185px">
+          <template slot-scope="scope">
+            <!--修改按钮-->
+            <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+            <!--删除按钮-->
+            <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+            <!--分配角色按钮-->
+            <el-tooltip class="item" effect="dark" content="分配角色" placement="top" :enterable="false">
+              <el-button type="warning" icon="el-icon-setting" size="mini"></el-button>
+            </el-tooltip>
+          </template>
+        </el-table-column>
+      </el-table>
+
+      <!--分页区域-->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pageId"
+        :page-sizes="[1, 2, 4, 6]"
+        :page-size="queryInfo.pageSize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total">
+      </el-pagination>
+
+      <!--添加上课记录-->
+      <el-dialog
+        title="添加上课记录" :visible.sync="addDialogVisible" width="50%">
+        <!--内容主题区-->
+        <el-form :model="addForm" label-width="70px">
+          <el-form-item label="上课地点ID" prop="locationId" label-width="100px">
+            <el-input v-model="addForm.locationId"></el-input>
+          </el-form-item>
+          <el-form-item label="课堂名称" prop="className" label-width="100px">
+            <el-input v-model="addForm.className"></el-input>
+          </el-form-item>
+          <el-form-item label="课堂类型ID" prop="classTypeId" label-width="100px">
+            <el-input v-model="addForm.classTypeId"></el-input>
+          </el-form-item>
+          <el-form-item label="课时" prop="classPeriod" label-width="100px">
+            <el-input v-model="addForm.classPeriod"></el-input>
+          </el-form-item>
+          <el-form-item label="上课具体时间" prop="classTime" label-width="100px">
+            <el-input v-model="addForm.classTime"></el-input>
+          </el-form-item>
+          <el-form-item label="时长" prop="duration" label-width="100px">
+            <el-input v-model="addForm.duration" placeholder="单位：分钟"></el-input>
+          </el-form-item>
+          <el-form-item label="课堂备注" prop="content" label-width="100px">
+            <el-input v-model="addForm.content"></el-input>
+          </el-form-item>
+          <el-form-item label="创建时间" prop="createTime" label-width="100px">
+            <el-input v-model="addForm.createTime"></el-input>
+          </el-form-item>
+        </el-form>
+        <!--底部区域-->
+        <span slot="footer" class="dialog-footer">
+            <el-button @click="addDialogVisible = false">取 消</el-button>
+            <el-button type="primary" @click="addClassRecord(addForm)">确 定</el-button>
+          </span>
+      </el-dialog>
+    </el-card>
+
+  </div>
+
 </template>
 
 <script>
-    export default {
+  import qs from 'qs'
+
+  export default {
+    data() {
+      return {
+        queryInfo: {
+          pageId: 1,
+          pageSize: 1
+        },
+        classRecordList: [],
+        total: 0,
+        // 控制添加上课记录对话框的显示与隐藏
+        addDialogVisible: false,
+        // 添加用户的表单数据
+        addForm: {
+          locationId: '',
+          className: '',
+          classTypeId: '',
+          classPeriod: '',
+          classTime: '',
+          duration: '',
+          content: '',
+          createTime: ''
+        }
+      }
+    },
+    created() {
+      this.getClassRecordList()
+    },
+    methods: {
+      async getClassRecordList() {
+        const {data: res} = await this.$http.get('yl/classrecords/getclassrecordlist',{
+          params: this.queryInfo
+        })
+        if (res.code !== 0) return this.$message.error('获取管理员信息失败！')
+        this.classRecordList = res.data.data
+        this.total = res.data.totalCount
+      },
+      // 时间格式化
+      dateFormat: function (row, column) {
+        var date = row[column.property]
+        if (date === undefined) {
+          return ''
+        }
+        return this.$moment(date).format('YYYY-MM-DD HH:mm:ss')
+      },
+      // 监听 pagesize 改变的事件
+      handleSizeChange(newSize) {
+        this.queryInfo.pageSize = newSize
+        this.getClassRecordList()
+      },
+      // 监听 页码值 改变
+      handleCurrentChange(newPage) {
+        this.queryInfo.pageId = newPage
+        this.getClassRecordList()
+      },
+      // 添加上课记录
+      async addClassRecord(addForm) {
+        const {data: res} = await this.$http.post('yl/classrecords/addclassrecord', qs.stringify({
+          locationId: addForm.locationId,
+          className: addForm.className,
+          classTypeId: addForm.classTypeId,
+          classPeriod: addForm.classPeriod,
+          // classTime: addForm.classTime,
+          duration: addForm.duration,
+          content: addForm.content
+          // createTime: addForm.createTime
+        }))
+        if (res.code !== 0) return this.$message.error('获取管理员信息失败！')
+        // this.addDialogVisible = false
+        this.$message.success('更新状态成功！')
+        console.log(res)
+      }
     }
+  }
 </script>
 
-<style scoped>
+<style lang="less" scoped>
 
 </style>
